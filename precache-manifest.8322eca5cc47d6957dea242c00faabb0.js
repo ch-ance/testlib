79e72455bff52f6925b02b845c1cdd116b44dedd
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2022-01-12 02:57:51 -0600

    Updates

---
+++ precache-manifest.8322eca5cc47d6957dea242c00faabb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f6752e9b5bf9bf13e37156b719c346",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b048a08b2f220a2b9421",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "eccf5e2b7f670ce6228e",
+    "url": "./static/js/2.872b29c3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.872b29c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b048a08b2f220a2b9421",
+    "url": "./static/js/main.ffd5fc45.chunk.js"
+  },
+  {
+    "revision": "fdc1b9ec6e8d38e14edd",
+    "url": "./static/js/runtime-main.cfa81312.js"
+  }
+]);
